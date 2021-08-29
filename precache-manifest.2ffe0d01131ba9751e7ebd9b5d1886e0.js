eaf6ed3a58ba7d68ab8c70936fa6a72ce29a8f53
Dmitriy Pleshakov <dmitriypleshakov1@gmail.com>
2021-08-29 17:56:15 +0300

    Updates

---
+++ precache-manifest.2ffe0d01131ba9751e7ebd9b5d1886e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e97ce889e1cbf2fa9b5646f8fde6f1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5a550f5119c87f297363",
+    "url": "./static/css/main.92049d81.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a550f5119c87f297363",
+    "url": "./static/js/main.e3b8448e.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
